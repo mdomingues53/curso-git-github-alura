41581f0f04d1e028f82befb1084b40634935ad09
Marcelo Domingues <mdomingues53@hotmail.com>
2023-12-14 17:14:48 -0300

    Incluindo mais uma linha de console

--- app.js
+++ app.js
@@ -1,2 +1,3 @@
-console.log("Rodando o sistema de gerenciamneto de jogos");
-console.log("Editando aruqivo app.js para teste do comando git pull");
+console.log('Rodando o sistema de gerenciamneto de jogos');
+console.log('Editando aruqivo app.js para teste do comando git pull');
+console.log('Incluindo mais uma linha de console');
